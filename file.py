2e5f2e57408eb0edef704e48973e4126cd9ccc15
alicexcai <85530513+alicexcai@users.noreply.github.com>
2022-04-01 15:44:14 -0400

    \" %s \"\n" , git_diff . message )

--- file.py
+++ file.py
@@ -1,7 +1,12 @@
 # This is a test python file.
 
 def print_text(text):
-    print(text[-1])
+    print(text)
 return
 
 print_text("Hello world")
+
+def add_numbers(*args):
+    return sum(args)
+
+add_numbers(1, 2, 3)
